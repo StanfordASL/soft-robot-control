72a71d7d996c5a5a842911c7bfcac5afd85867cc
jjalora <jjalora@stanford.edu>
2022-04-23 16:01:14 -0700

    Shifted decay trajectories to the origin.

--- examples/diamond/SSM_sysID.py
+++ examples/diamond/SSM_sysID.py
@@ -10,8 +10,14 @@ path = dirname(abspath(__file__))
 
 # Load SCP data
 decay_simdata_file = join(path, 'decay_snapshots.pkl')
+rest_file = join(path, 'rest.pkl')
+
 decay_data = load_data(decay_simdata_file)
+rest_data = load_data(rest_file)
 t_decay = decay_data['t']
-z_scp = decay_data['z']
-solve_times_scp = decay_data['info']['solve_times']
-real_time_limit_scp = decay_data['info']['rollout_time']
+q_decay = decay_data['q']
+q_equilibrium = rest_data['rest']
+
+q_shift_decay = np.array([q_decay - q_equilibrium])
+
+print('Done')
